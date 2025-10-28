8d17d6881a74ba95c4b14001d50ac0775a70b3db
Sahil Alam <160860074@ggi.ac.in>
2025-10-28 23:17:32 +0530

    Add files via upload

---
+++ LinearSearch.cpp
@@ -0,0 +1,20 @@
+/*Linear search
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int target,i;
+    cout<<"Enter the target element:";
+    cin>>target;
+    for(int i=0;i<5;i++){
+        if(arr[i]==target){
+            cout<<"Element found at index:"<<i;
+            break;
+        }
+    }
+    if(i==5){
+        cout<<"Element is not found:";
+    }
+    return 0;
+}*/
